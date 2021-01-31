0de30ccaafbe3bf9de4e1180ef2085278bbd6561
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2021-01-30 23:15:51 -0300

    day 4: resolving in my own super way some arrays excercises

---
+++ 9 -  Interview Questions/crackingthecode/src/crackingthecode/Ex1.java
@@ -0,0 +1,15 @@
+package crackingthecode;
+
+public class Ex1 {
+
+
+//		Is Unique: Implement an algorithm to determine if a string has all unique characters. 
+//		What if you cannot use additional data structures? 
+	
+	public String evalString(String myString) {
+		
+		return "";
+	}
+
+
+}
---
+++ 9 -  Interview Questions/crackingthecode/src/crackingthecode/Main.java
@@ -0,0 +1,10 @@
+package crackingthecode;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
