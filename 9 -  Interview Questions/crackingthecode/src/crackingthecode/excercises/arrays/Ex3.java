33153f6abdbdb3952f711447ce74efb371643e5d
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2021-02-02 23:49:45 -0300

    I'm tired of O(N)

--- 9 -  Interview Questions/crackingthecode/src/crackingthecode/excercises/arrays/Ex3.java
+++ 9 -  Interview Questions/crackingthecode/src/crackingthecode/excercises/arrays/Ex3.java
@@ -1,5 +1,24 @@
 package crackingthecode.excercises.arrays;
 
 public class Ex3 {
+	
+	public String[] urlify(String string, int len) {
+		
+		String[] sArray = string.split("");
+		
+		for(int i = 0; i < len; i++) {
+			if(sArray[i].equals(" ")) {
+				sArray[i] = "%20";
+			}
+		}
+//        
+//		for(int i = 0; i < len; i++) {
+//			System.out.println(sArray[i]);
+//		}
+
+		return sArray;
+	}
+	
+//	Un bonito O(n)
 
 }
---
+++ 9 -  Interview Questions/crackingthecode/src/crackingthecode/excercises/arrays/Main3.java
@@ -0,0 +1,14 @@
+package crackingthecode.excercises.arrays;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+
+		
+		Ex3 ex3 = new Ex3();
+
+		System.out.println(ex3.urlify("Mr John Smith", 13));
+	}
+
+
+}
